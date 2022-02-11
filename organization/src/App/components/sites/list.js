0be62f60b95a5d8693adf7b96147c299f4fb5b74
Saravanan Pandiaraj <saravanan.pandiaraj@bitcontroltech.com>
2022-02-11 20:59:30 +0530

    incident image list in model

--- organization/src/App/components/sites/list.js
+++ organization/src/App/components/sites/list.js
@@ -73,10 +73,10 @@ function SiteList(props) {
                       <div onClick={() => handleShow(data.id)} className="ml-10 cursor-pointer">
                        <i className="fa fa-trash-o" aria-hidden="true"></i>
                       </div>
-                       <Link to={`/admin/clients/${client_id}/site/${data.id}/guests`} className="ml-10 cursor-pointer">
+                       <Link to={`/admin/clients/${client_id}/site/${data.id}/guests`} className="ml-10 cursor-pointer link-list">
                         Guests
                       </Link>
-                       <Link to={`/admin/clients/${client_id}/site/${data.id}/reports`} className="ml-10 cursor-pointer">
+                       <Link to={`/admin/clients/${client_id}/site/${data.id}/reports`} className="ml-10 cursor-pointer link-list">
                         Reports
                       </Link>
                     </div>
--- organization/src/App/components/sites/reports.js
+++ organization/src/App/components/sites/reports.js
@@ -1,35 +1,47 @@
-import React, {useState, useEffect} from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { connect, useDispatch } from 'react-redux';
-import { Table} from 'react-bootstrap';
-import { siteActions } from '../../../actions';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { connect, useDispatch } from "react-redux";
+import { Table, Modal, Button } from "react-bootstrap";
+import { siteActions } from "../../../actions";
+import moment from "moment";
 
 function SiteList(props) {
   const dispatch = useDispatch();
   const { client_id, site_id } = useParams();
-  const { reports } = props
+  const { reports } = props;
+  const [show, setShow] = useState(false);
+  const [incidentImages, setIncidentImages] = useState();
 
   useEffect(() => {
     dispatch(siteActions.historyReports(site_id));
   }, []);
+
+  const handleClose = () => setShow(false);
+
+  const handleShow = (incidentImages) => {
+    setShow(true);
+    setIncidentImages(incidentImages);
+  };
   return (
-   <div  className="container">
+    <div className="container">
       <div className="page_header d-flex align-items-center justify-content-between py-2">
         <h5>Incident Reports History</h5>
       </div>
       <Table bordered size="sm responsive">
         <thead>
           <tr>
-            <th className="text-center" style={{width: '3%'}}>S.No</th>
-            <th width="20%">Incident Type</th>
+            <th className="text-center" style={{ width: "3%" }}>
+              S.No
+            </th>
+            <th width="15%">Incident Type</th>
             <th width="20%">Summary</th>
-            <th width="40%">Description</th>
+            <th width="30%">Description</th>
             <th width="20%">Created at</th>
+            <th width="20%"></th>
           </tr>
         </thead>
         <tbody>
-          {reports && reports.length > 0 ? 
+          {reports && reports.length > 0 ? (
             reports.map((data, index) => {
               return (
                 <tr key={data.id}>
@@ -37,17 +49,63 @@ function SiteList(props) {
                   <td>{data.incident_type}</td>
                   <td>{data.summary}</td>
                   <td>{data.description}</td>
-                   <td>{moment(data.created_at).format('DD-MM-YYYY hh:mm A')}</td>
+                  <td>
+                    {moment(data.created_at).format("DD-MM-YYYY hh:mm A")}
+                  </td>
+                  <td>
+                    <div onClick={() => handleShow(data.incident_images)} style={{color: 'blue', textAlign: 'center', cursor: 'pointer'}}>
+                      {" "}
+                      Incident Images{" "}
+                    </div>
+                  </td>
                 </tr>
-              )
+              );
             })
-            : 
+          ) : (
             <td colSpan="20" className="text-center">
               <div className="py-3">No data yet</div>
             </td>
-          }
+          )}
         </tbody>
       </Table>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title style={{ fontSize: 18 }}>
+            Report Incident Images
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          style={{
+            maxHeight: "calc(100vh - 210px)",
+            overflowY: "auto",
+          }}
+        >
+          <div className="row">
+            {incidentImages &&
+              incidentImages.map((data, i) => {
+                return (
+                  <div key={data.id} className="col-md-12">
+                    <img
+                      src={data.image.url}
+                      alt="user pic"
+                      style={{ width: "100%", marginBottom: 10 }}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
@@ -55,8 +113,8 @@ function SiteList(props) {
 const mapStateToProps = (state) => {
   return {
     loading: state.site.loading,
-    reports: state.site.reports
+    reports: state.site.reports,
   };
 };
 
-export default connect(mapStateToProps, {siteActions })(SiteList);
+export default connect(mapStateToProps, { siteActions })(SiteList);
